d7fe45a1043e784a417bccbab59f955d247d9cf6
hackmgrm <hackmgrm@gmail.com>
2023-06-05 20:52:47 +0800

    Update qjmfxs.js

--- qjmfxs.js
+++ qjmfxs.js
@@ -3,7 +3,7 @@
 
 [rewrite_local]
 http-response ^https:\//appi.qijizuopin.com\/app_v1\/user\/vip\/getuserinfo url script-response-body https://raw.githubusercontent.com/hackmgrm/test/main/qjmfxs.js
-http-response ^https:\//appi.qijizuopin.com\/app_v2\/mymember url script-response-body https://raw.githubusercontent.com/hackmgrm/test/main/qjmfxs2.js
+http-response ^https:\//appi.qijizuopin.com\/app_v2\/mymember url script-response-body https://raw.githubusercontent.com/hackmgrm/test/main/qjmfxs.js
 
 
 [MITM]
@@ -12,5 +12,5 @@ hostname = appi.qijizuopin.com
 */
 
 var body = $response.body
-    .replace(/\"data\":\{\"userVipType\":0,\"isUserVip\":false,\"userVipLevel\":-1,\"createDate\":\"\",\"expiredTime\":\"\"\}/, "\"data\":{\"userVipType\":1,\"isUserVip\":true,\"userVipLevel\":3,\"createDate\":\"\",\"expiredTime\":1894118400\}");
+    .replace(/\"data\":\{\"userVipType\":0,\"isUserVip\":false,\"userVipLevel\":-1,\"expiredTime\":0,}/, "\"data\":{\"userVipType\":1,\"isUserVip\":true,\"userVipLevel\":3,\"expiredTime\":1894118400}");
 $done({ body });
