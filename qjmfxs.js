d3bb9439701e8c50522f91b63d8c09682693feec
hackmgrm <hackmgrm@gmail.com>
2023-06-05 20:27:08 +0800

    Update qjmfxs.js

--- qjmfxs.js
+++ qjmfxs.js
@@ -3,7 +3,7 @@
 
 [rewrite_local]
 http-response ^https:\//appi.qijizuopin.com\/app_v1\/user\/vip\/getuserinfo url script-response-body https://raw.githubusercontent.com/hackmgrm/test/main/qjmfxs.js
-http-response ^https:\//appi.qijizuopin.com\/app_v2\/mymember url script-response-body https://raw.githubusercontent.com/hackmgrm/test/main/qjmfxs.js
+http-response ^https:\//appi.qijizuopin.com\/app_v2\/mymember url script-response-body https://raw.githubusercontent.com/hackmgrm/test/main/qjmfxs2.js
 
 
 [MITM]
