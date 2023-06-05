6d3193e348a35bcdf0807a0bb1b0c5605c9cfe99
hackmgrm <hackmgrm@gmail.com>
2023-06-05 20:09:47 +0800

    Update qjmfxs.js

--- qjmfxs.js
+++ qjmfxs.js
@@ -3,6 +3,8 @@
 
 [rewrite_local]
 http-response ^https:\//appi.qijizuopin.com\/app_v1\/user\/vip\/getuserinfo url script-response-body https://raw.githubusercontent.com/hackmgrm/test/main/qjmfxs.js
+http-response ^https:\//appi.qijizuopin.com\/app_v2\/mymember url script-response-body https://raw.githubusercontent.com/hackmgrm/test/main/qjmfxs.js
+
 
 [MITM]
 hostname = appi.qijizuopin.com
@@ -11,5 +13,4 @@ hostname = appi.qijizuopin.com
 
 var body = $response.body
     .replace(/\"data\":\{\"userVipType\":0,\"isUserVip\":false,\"userVipLevel\":-1,\"createDate\":\"\",\"expiredTime\":\"\"\}/, "\"data\":{\"userVipType\":1,\"isUserVip\":true,\"userVipLevel\":3,\"createDate\":\"\",\"expiredTime\":1894118400\}");
-
 $done({ body });
