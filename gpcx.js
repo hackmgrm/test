cc3877d141b36b4cf279f16281a2ab0dcbf306b5
hackmgrm <hackmgrm@gmail.com>
2026-01-13 16:07:53 +0800

    Update gpcx.js

--- gpcx.js
+++ gpcx.js
@@ -1,17 +1,11 @@
-// stock_user.js
-let url = "https://stock.53site.com/RecommendStock/v3.5.4/adduser.php";
+let obj = JSON.parse($response.body);
 
-$httpClient.get(url, function(error, response, data) {
-  if (error) {
-    $notification.post("股票账户状态", "请求失败", error);
-  } else {
-    try {
-      let obj = JSON.parse(data);
-      let msg = `用户号: ${obj.userNo}\n状态: ${obj.buystate === "1" ? "✅ 已购买" : "❌ 未购买"}`;
-      $notification.post("股票账户状态", "查询成功", msg);
-    } catch (e) {
-      $notification.post("股票账户状态", "解析失败", e.message);
-    }
-  }
-  $done();
-});
+// 通知提示
+let msg = `用户号: ${obj.userNo}\n状态: ${obj.buystate === "1" ? "✅ 已购买" : "❌ 未购买"}`;
+$notification.post("股票账户状态", "查询成功", msg);
+
+// 如果要伪造返回结果，可以直接修改
+obj.buystate = "1";
+obj.message = "账户已购买";
+
+$done({body: JSON.stringify(obj)});
