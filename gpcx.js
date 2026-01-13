f23a4c211fc7a24863b492cf8f0f98f761fb9cc1
hackmgrm <hackmgrm@gmail.com>
2026-01-13 16:31:23 +0800

    Update gpcx.js

--- gpcx.js
+++ gpcx.js
@@ -1,20 +1,17 @@
-// gpcx.js
-// 
-
 let obj = {
   "sign": "1",
   "buystate": "1",
   "message": "账户已购买",
-  "transaction_id": "1",
+  "transaction_id": "20260113" + Math.floor(Math.random() * 1000000),
   "buy_apns_state": "1",
-  "transaction_id_apns": "240002147952008",
+  "transaction_id_apns": "24000" + Math.floor(100000000 + Math.random() * 900000000),
   "buy_teacher_state": "1",
-  "transaction_id_teacher": "1",
+  "transaction_id_teacher": "T" + Math.floor(1000000 + Math.random() * 9000000),
   "apns_switch": "1",
   "darkhorse_switch": "1",
   "stocknotice_switch": "1",
-  "latesDate": "2026-01-12",
-  "userNo": "2307776"
+  "latesDate": "2028-01-13",          // ← 改这里，建议每年更新一次
+  "userNo": "2307776"                 // 如果 App 不校验，可保留；否则可删掉或随机
 };
 
 $done({body: JSON.stringify(obj)});
