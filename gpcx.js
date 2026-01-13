2a29771b7e8098637618b84d636e71be827bced2
hackmgrm <hackmgrm@gmail.com>
2026-01-13 16:11:11 +0800

    Update gpcx.js

--- gpcx.js
+++ gpcx.js
@@ -1,11 +1,20 @@
-let obj = JSON.parse($response.body);
+// gpcx.js
+// 
 
-// 通知提示
-let msg = `用户号: ${obj.userNo}\n状态: ${obj.buystate === "1" ? "✅ 已购买" : "❌ 未购买"}`;
-$notification.post("股票账户状态", "查询成功", msg);
-
-// 如果要伪造返回结果，可以直接修改
-obj.buystate = "1";
-obj.message = "账户已购买";
+let obj = {
+  "sign": "1",
+  "buystate": "1",
+  "message": "账户已购买",
+  "transaction_id": "1",
+  "buy_apns_state": "1",
+  "transaction_id_apns": "240002147952008",
+  "buy_teacher_state": "1",
+  "transaction_id_teacher": "1",
+  "apns_switch": "1",
+  "darkhorse_switch": "1",
+  "stocknotice_switch": "1",
+  "latesDate": "2026-01-12",
+  "userNo": "2307776"
+};
 
 $done({body: JSON.stringify(obj)});
